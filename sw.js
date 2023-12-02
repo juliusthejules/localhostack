2ff5efe83e7864812338173cfb56711a777239e7
Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>
2023-12-02 02:39:47 -0600

    Update sw.js
    Signed-off-by: Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,22 +1,20 @@
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+/* Copyright 2015, 2019 Google Inc. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
 */
 
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
+
 // Customize this with a different URL if needed.
-const OFFLINE_URL = 'index.html';
+const OFFLINE_URL = './index.html';
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
@@ -51,7 +49,6 @@ self.addEventListener('fetch', (event) => {
         if (preloadResponse) {
           return preloadResponse;
         }
-
         const networkResponse = await fetch(event.request);
         return networkResponse;
       } catch (error) {
@@ -60,14 +57,12 @@ self.addEventListener('fetch', (event) => {
         // If fetch() returns a valid HTTP response with a response code in
         // the 4xx or 5xx range, the catch() will NOT be called.
         console.log('Fetch failed; returning offline page instead.', error);
-
         const cache = await caches.open(CACHE_NAME);
         const cachedResponse = await cache.match(OFFLINE_URL);
         return cachedResponse;
       }
     })());
   }
-
   // If our if() condition is false, then this fetch handler won't intercept the
   // request. If there are any other fetch handlers registered, they will get a
   // chance to call event.respondWith(). If no fetch handlers call
