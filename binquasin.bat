c682b9aaaccc95d630d0ce35f110d4c865d5f004
Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>
2023-12-21 16:34:15 -0600

    Update binquasin.bat
    Signed-off-by: Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>

--- binquasin.bat
+++ binquasin.bat
@@ -1,1 +1,1 @@
-@ECHO OFF REM Device identification REM Hide device IP address ROUTE ADD DEFAULT GATEWAY 192.168.1.1 REM Hide device MAC address MACCHANGER -R wlan0 REM Hide device Bluetooth addresses BLUETOOTHCTL POWER OFF REM Hide IP address hostname NETDOM RENAME %COMPUTERNAME% localhost
+@ECHO OFF REM Device identification REM Hide device IP address ROUTE ADD DEFAULT GATEWAY 127.0.0.1 REM Hide device MAC address MACCHANGER -R wlan0 REM Hide device Bluetooth addresses BLUETOOTHCTL POWER OFF REM Hide IP address hostname NETDOM RENAME %COMPUTERNAME% 127.0.0.1
