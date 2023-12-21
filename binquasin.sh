eea9f3409710e7b186da342557e8f5390ed1cdf9
Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>
2023-12-21 16:33:38 -0600

    Update binquasin.sh
    Signed-off-by: Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>

--- binquasin.sh
+++ binquasin.sh
@@ -3,10 +3,10 @@ chmod +x ./binquasin.sh
 
 # Device identification
 # Hide device IP address
-ip route add default via 192.168.1.1
+ip route add default via 127.0.0.1
 # Hide device MAC address
 macchanger -r wlan0
 # Hide device Bluetooth addresses
 bluetoothctl power off
 # Hide IP address hostname
-hostnamectl set-hostname "localhost"
+hostnamectl set-hostname "127.0.0.1"
