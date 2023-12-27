5b8c3176ae1b51c8f14e20efb8be82b344993cff
Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>
2023-12-27 08:29:49 -0600

    Update sw.js
    Signed-off-by: Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -8,64 +8,48 @@ CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
 const CACHE_NAME = 'offline';
 
-// Customize this with a different URL if needed.
-const OFFLINE_URL = 'https://juliusthejules.github.io/localhostack/index.html';
+// Replace with the base URL of your GitHub Pages site:
+const BASE_URL = 'https://juliusthejules.github.io/localhostack';
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+    await cache.add(new Request(BASE_URL + '/index.html', { cache: 'reload' }));
   })());
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
     if ('navigationPreload' in self.registration) {
       await self.registration.navigationPreload.enable();
     }
   })());
-
-  // Tell the active service worker to take control of the page immediately.
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
   if (event.request.mode === 'navigate') {
     event.respondWith((async () => {
       try {
-        // First, try to use the navigation preload response if it's supported.
         const preloadResponse = await event.preloadResponse;
         if (preloadResponse) {
           return preloadResponse;
         }
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
+
+        // Only fetch requests for the GitHub Pages site
+        if (event.request.url.startsWith(BASE_URL)) {
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        }
       } catch (error) {
-        // catch is only triggered if an exception is thrown, which is likely
-        // due to a network error.
-        // If fetch() returns a valid HTTP response with a response code in
-        // the 4xx or 5xx range, the catch() will NOT be called.
         console.log('Fetch failed; returning offline page instead.', error);
         const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
+        const cachedResponse = await cache.match(BASE_URL + '/index.html');
         return cachedResponse;
       }
     })());
   }
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
 });
