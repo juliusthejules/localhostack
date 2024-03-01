9c3fa2259302457e5c4ce00fe2de649330b6e875
Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>
2024-02-29 18:39:56 -0600

    Update sw.js
    Signed-off-by: Joseph D. Smith <142750447+juliusthejules@users.noreply.github.com>

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/localhostack';
 var APP_PREFIX = 'localhostack_';
-var VERSION = 'version_001';
+var VERSION = 'version_002';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
